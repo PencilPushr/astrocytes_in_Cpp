3909c96d7944e0891f9d6e4fa15d5c1fe58f856f
PencilPushr <ajvallee15@gmail.com>
2024-10-24 19:21:01 +0100

    Finalised all the reactions

--- src/algorithm/reactions.cpp
+++ src/algorithm/reactions.cpp
@@ -1,46 +1,224 @@
+// reactions.cpp
 #include "reactions.h"
-#include "../../util/includes.h"
 
 Reactions::Reactions(const std::vector<double>& ini, int number_of_rec, int number_of_var)
-    : ini(ini), number_of_rec(number_of_rec), number_of_var(number_of_var)
+    : ini(ini), number_of_rec(number_of_rec), number_of_var(number_of_var) 
+{}
+
+double Reactions::calculate_sigma0(int i) 
 {
+    // ini[(i/numberofrec)*numberofvar+0]*ini[(i/numberofrec)*numberofvar+1]
+    return rec(i, FormulaVariables::v1) * rec(i, FormulaVariables::Y);
 }
 
-double Reactions::calculate_sigma0(int i)
+double Reactions::calculate_sigma1(int i) 
 {
-    return rec(i, 2);
+    return rec(i, FormulaVariables::vin);
 }
 
-double Reactions::calculate_sigma1(int i)
+double Reactions::calculate_sigma2(int i) 
 {
-    double sigmaM3_4    = 4 * rec(i,            FormulaVariables::sigmaEM3);
-    double kC1_n        = std::pow(rec(i,       FormulaVariables::kC1), rec(i, FormulaVariables::n));
-    double kC2_n        = std::pow(rec(i,       FormulaVariables::kC2), rec(i, FormulaVariables::n));
-    double Ca           = rec(i,                FormulaVariables::kCa);
-    double Ca_n         = std::pow(Ca, rec(i,   FormulaVariables::n));
-    double Ia_m         = std::pow(rec(i,       FormulaVariables::Ia), rec(i, FormulaVariables::m));
-    double kI_m         = std::pow(rec(i,       FormulaVariables::KI), rec(i, FormulaVariables::m));
-    double E_a          = rec(i,                FormulaVariables::Ea);
+    using namespace FormulaVariables;
 
-    double numerator1 = sigmaM3_4 * (kC1_n * Ca_n);
-    double denominator1 = (Ca_n + kC1_n) * (Ca_n + kC2_n);
+    double VM3_val = rec(i, VM3);
+    double n_val = rec(i, n);
+    double Ca_val = rec(i, Ca);
+    double kcaaa_val = rec(i, kcaaa);
+    double kcai_val = rec(i, kcai);
+    double Ia_val = rec(i, Ia);
+    double m_val = rec(i, m);
+    double kip3_val = rec(i, kip3);
+    double S_val = rec(i, S);
 
-    double numerator2 = Ia_m;
-    double denominator2 = kI_m + Ia_m;
+    // Calculate power terms
+    double Ca_n = std::pow(Ca_val, n_val);
+    double kcaaa_n = std::pow(kcaaa_val, n_val);
+    double kcai_n = std::pow(kcai_val, n_val);
+    double Ia_m = std::pow(Ia_val, m_val);
+    double kip3_m = std::pow(kip3_val, m_val);
 
-    double right = numerator1 / denominator1;
-    double left = (numerator2 / denominator2) * (E_a - Ca);
+    // Calculate the complex formula parts
+    double term1 = 4 * VM3_val * (kcaaa_n * Ca_n);
+    double term2 = (Ca_n + kcaaa_n) * (Ca_n + kcai_n);
+    double term3 = Ia_m / (Ia_m + kip3_m);
 
-    return right * left;
+    return (term1 / term2) * term3 * (S_val - Ca_val);
 }
 
-double Reactions::calculate_sigma2(int i)
+double Reactions::calculate_sigma3(int i) 
 {
-    // Implement the calculation for sigma2
-    return 0;
+    using namespace FormulaVariables;
+
+    double VM2_val = rec(i, VM2);
+    double Ca_val = rec(i, Ca);
+    double K2_val = rec(i, K2);
+
+    double Ca_squared = Ca_val * Ca_val;
+    double K2_squared = K2_val * K2_val;
+
+    return VM2_val * (Ca_squared / (Ca_squared + K2_squared));
 }
 
-double Reactions::rec(int i, int x)
+double Reactions::calculate_sigma4(int i) {
+    using namespace FormulaVariables;
+    return rec(i, kf) * rec(i, S);
+}
+
+double Reactions::calculate_sigma5(int i) 
+{
+    using namespace FormulaVariables;
+    return rec(i, kf) * rec(i, Ca);
+}
+
+double Reactions::calculate_sigma6(int i) 
+{
+    using namespace FormulaVariables;
+    return rec(i, kout) * rec(i, Ca);
+}
+
+double Reactions::calculate_sigma7(int i) 
+{
+    using namespace FormulaVariables;
+
+    double vp_val = rec(i, vp);
+    double vin_val = rec(i, vin);
+    double Ca_val = rec(i, Ca);
+    double kp_val = rec(i, kp);
+
+    double vin_squared = vin_val * vin_val;
+    double Ca_squared = Ca_val * Ca_val;
+    double kp_squared = kp_val * kp_val;
+
+    return vp_val * (vin_squared / (Ca_squared + kp_squared));
+}
+
+double Reactions::calculate_sigma8(int i) 
+{
+    using namespace FormulaVariables;
+    return rec(i, kdeg) * rec(i, Ia);
+}
+
+double Reactions::calculate_sigma9(int i) 
+{
+    using namespace FormulaVariables;
+
+    double ka_val = rec(i, ka);
+    double Ca_val = rec(i, Ca);
+    double q_val = rec(i, q);
+    double K_val = rec(i, K);
+    double W_val = rec(i, W);
+    double A_val = rec(i, A);
+
+    double Ca_q = std::pow(Ca_val, q_val);
+    double K_q = std::pow(K_val, q_val);
+
+    return ka_val * (Ca_q / (K_q + Ca_q)) * (W_val - 0 * A_val);
+}
+
+double Reactions::calculate_base_diffusion(int i, int target_idx) 
+{
+    using namespace FormulaVariables;
+
+    double D_val = rec(i, D);
+    double l_val = rec(i, l);
+    double Ca_current = rec(i, Ca);
+    double Ca_target = ini[target_idx * number_of_var + Ca];
+
+    if (Ca_current <= Ca_target) {
+        return 0.0;
+    }
+
+    return (D_val / l_val) * std::fabs(Ca_current - Ca_target);
+}
+
+double Reactions::calculate_diffusion_x_positive(int i) 
+{
+    int target_idx = i + 1;
+    if (target_idx >= ini.size() / number_of_var) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::calculate_diffusion_x_negative(int i) 
+{
+    int target_idx = i - 1;
+    if (target_idx < 0) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::calculate_diffusion_y_positive(int i) 
+{
+    // Need grid width for y direction calculation
+    int grid_width = std::sqrt(ini.size() / number_of_var);
+    int target_idx = i + grid_width;
+    if (target_idx >= ini.size() / number_of_var) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::calculate_diffusion_y_negative(int i) 
+{
+    int grid_width = std::sqrt(ini.size() / number_of_var);
+    int target_idx = i - grid_width;
+    if (target_idx < 0) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::calculate_diffusion_z_positive(int i) 
+{
+    int grid_area = ini.size() / (number_of_var * 3); // Assuming 3 layers
+    int target_idx = i + grid_area;
+    if (target_idx >= ini.size() / number_of_var) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::calculate_diffusion_z_negative(int i) 
+{
+    int grid_area = ini.size() / (number_of_var * 3); // Assuming 3 layers
+    int target_idx = i - grid_area;
+    if (target_idx < 0) return 0.0;
+
+    double base = calculate_base_diffusion(i, target_idx);
+    return base * rec(i, FormulaVariables::phh);
+}
+
+double Reactions::rec(int i, int x) 
 {
     return ini[(i / number_of_rec) * number_of_var + x];
+}
+
+std::vector<double> Reactions::get_all_propensities(int i) 
+{
+    std::vector<double> props;
+    props.reserve(number_of_rec);
+
+    // Add all reaction propensities
+    props.push_back(calculate_sigma0(i));
+    props.push_back(calculate_sigma1(i));
+    props.push_back(calculate_sigma2(i));
+    props.push_back(calculate_sigma3(i));
+    props.push_back(calculate_sigma4(i));
+    props.push_back(calculate_sigma5(i));
+    props.push_back(calculate_sigma6(i));
+    props.push_back(calculate_sigma7(i));
+    props.push_back(calculate_sigma8(i));
+    props.push_back(calculate_sigma9(i));
+
+    // Add diffusion reactions
+    props.push_back(calculate_diffusion_x_positive(i));
+    props.push_back(calculate_diffusion_x_negative(i));
+    props.push_back(calculate_diffusion_y_positive(i));
+    props.push_back(calculate_diffusion_y_negative(i));
+    props.push_back(calculate_diffusion_z_positive(i));
+    props.push_back(calculate_diffusion_z_negative(i));
+
+    return props;
 }
--- src/algorithm/reactions.h
+++ src/algorithm/reactions.h
@@ -1,37 +1,79 @@
-#ifndef REACTIONS_H
-#define REACTIONS_H
+// reactions.h
+#pragma once
 
-#include <vector> 
+#include <vector>
+#include <cmath>
+#include <array>
 
-namespace FormulaVariables
-{
-    static int kCa = 4;
-    static int n = 5;
-    static int sigmaEM3 = 7;
-    static int Ea = 9;
-    static int kC1 = 14;
-    static int kC2 = 15;
-    static int KI = 16;
-    static int Ia = 17;
-    static int m = 26;
+namespace FormulaVariables {
+    enum Variables {
+        v1 = 0,     // 0v1
+        Y = 1,      // 1Y
+        vin = 2,    // 2vin
+        VM2 = 3,    // 3VM2
+        Ca = 4,     // 4C
+        n = 5,      // 5n
+        K2 = 6,     // 6K2
+        VM3 = 7,    // 7VM3
+        kout = 8,   // 8kout
+        S = 9,      // 9S
+        kf = 10,    // 10kf
+        kp = 11,    // 11kp
+        kdeg = 12,  // 12kdeg
+        vp = 13,    // 13vp
+        kcaaa = 14, // 14kcaaa
+        kcai = 15,  // 15kcai
+        kip3 = 16,  // 16kip3
+        Ia = 17,    // 17Z
+        q = 18,     // 18q
+        W = 19,     // 19W
+        A = 20,     // 20A
+        kia = 21,   // 21kia
+        D = 22,     // 22D
+        l = 23,     // 23l
+        K = 24,     // 24K
+        ka = 25,    // 25ka
+        m = 26,     // 26m
+        phh = 27,   // 27phh
+        phl = 28,   // 28phl
+        plh = 29    // 29plh
+    };
 };
 
-class Reactions
-{
+class Reactions {
 public:
     Reactions(const std::vector<double>& ini, int number_of_rec, int number_of_var);
 
-    double calculate_sigma0(int i);
-    double calculate_sigma1(int i);
-    double calculate_sigma2(int i);
+    // Core reaction calculations
+    double calculate_sigma0(int i);  // Reaction 0
+    double calculate_sigma1(int i);  // Reaction 1
+    double calculate_sigma2(int i);  // Reaction 2
+    double calculate_sigma3(int i);  // Reaction 3
+    double calculate_sigma4(int i);  // Reaction 4
+    double calculate_sigma5(int i);  // Reaction 5
+    double calculate_sigma6(int i);  // Reaction 6
+    double calculate_sigma7(int i);  // Reaction 7
+    double calculate_sigma8(int i);  // Reaction 8
+    double calculate_sigma9(int i);  // Reaction 9 (Receptor reactions)
+
+    // Diffusion reactions
+    double calculate_diffusion_x_positive(int i);  // C_x,y,z -> C_x+1,y,z
+    double calculate_diffusion_x_negative(int i);  // C_x,y,z -> C_x-1,y,z
+    double calculate_diffusion_y_positive(int i);  // C_x,y,z -> C_x,y+1,z
+    double calculate_diffusion_y_negative(int i);  // C_x,y,z -> C_x,y-1,z
+    double calculate_diffusion_z_positive(int i);  // C_x,y,z -> C_x,y,z+1
+    double calculate_diffusion_z_negative(int i);  // C_x,y,z -> C_x,y,z-1
+
+    // Helper functions
+    std::vector<double> get_all_propensities(int i);
 
 private:
     double rec(int i, int x);
+    double calculate_base_diffusion(int i, int target_idx);
 
 private:
     const std::vector<double>& ini;
     int number_of_rec;
     int number_of_var;
+    static constexpr double ALPHA = 0.01;
 };
-
-#endif // REACTIONS_H
